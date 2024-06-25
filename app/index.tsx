7754f30f5931d5b32bce3040700de02def7315ca
Stan Chen <stanchensz@gmail.com>
2024-06-24 23:36:09 -0600

    wip

--- app/index.tsx
+++ app/index.tsx
@@ -7,13 +7,15 @@ export default function HomePage() {
 
   return (
     <div className="container text-center">
+      <img src="/index-img-01.jpg" alt="Welcome Image" className="my-4" style={{ width: '30%', height: 'auto' }} />
       <h1 className="my-4">Welcome to Techcademy</h1>
       <p className="lead">
         Techcademy is an open-source courseware framework that allows people to create courses and share them like a GitHub repo. Course designers have full flexibility to monetize or keep it completely open and free.
       </p>
-      <button className="btn btn-primary" onClick={() => router.push('/render')}>
-        Go to course index
+      <button className="btn btn-primary" onClick={() => router.push('/openai')}>
+        Explore Courses
       </button>
+
     </div>
   );
 }
